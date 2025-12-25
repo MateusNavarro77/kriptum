cbb48ccb00bd69f774648085cc1f97b733a41d26
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-12-25 13:53:52 -0300

    refactor: update Ethereum address validator initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kriptum/blocs/theme/theme_bloc.dart';
 import 'package:kriptum/config/di/injector.dart';
+import 'package:kriptum/domain/value_objects/ethereum_address/ethereum_address.dart';
 import 'package:kriptum/infra/persistence/database/sql_database.dart';
 import 'package:kriptum/infra/persistence/user_preferences/user_preferences.dart';
+import 'package:kriptum/infra/validators/validators.dart';
 import 'package:kriptum/ui/app.dart';
 
 Future<void> main(List<String> args) async {
@@ -13,6 +15,7 @@ Future<void> main(List<String> args) async {
   await injector.get<SqlDatabase>().initialize();
   final isDarkMode = await injector.get<UserPreferences>().isDarkModeEnabled();
   final currentTheme = isDarkMode ? ThemeDark() : ThemeLight();
+  EthereumAddress.setExternalValidator(Web3EthereumAddressValidatorImpl());
   runApp(
     MultiBlocProvider(
       providers: [
