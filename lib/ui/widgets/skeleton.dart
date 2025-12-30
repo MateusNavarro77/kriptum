a69c9b5fcb529441d294a9336caeb25756c0f46a
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-12-30 20:36:00 -0300

    feat: add Skeleton widget for loading state representation

---
+++ lib/ui/widgets/skeleton.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class Skeleton extends StatelessWidget {
+  final bool isLoading;
+  final Widget child;
+  const Skeleton({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Skeletonizer(
+      
+      enabled: isLoading,
+      effect: PulseEffect(
+        from: Colors.grey.shade300,
+        to: Colors.grey.shade100,
+      ),
+      child: child,
+    );
+  }
+}
